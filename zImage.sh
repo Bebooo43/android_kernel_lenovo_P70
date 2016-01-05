4009b0aba20344208307ac801c9970777891f811
vgdn1942 <vgdn1942@gmail.com>
2016-01-05 23:56:44 +0300

    Update build script

--- zImage.sh
+++ zImage.sh
@@ -5,3 +5,4 @@ make O=out ARCH=arm64 CROSS_COMPILE=~/gcc/gcc-linaro-5.2-aarch64-linux-gnu/bin/a
 make -j2 O=out ARCH=arm64 CROSS_COMPILE=~/gcc/gcc-linaro-5.2-aarch64-linux-gnu/bin/aarch64-linux-gnu-
 cp ~/kernel-3.10/out/arch/arm64/boot/Image.gz-dtb ~/kernel-3.10/out/arch/arm64/boot/boot.img-kernel
 cp ~/kernel-3.10/out/arch/arm64/boot/boot.img-kernel ~/Kitchen/boot/boot.img-kernel
+cd
