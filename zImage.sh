ed7ced75bb62a01147ae1613cc5ddf2e3e0d3601
vgdn1942 <vgdn1942@gmail.com>
2016-01-18 17:13:26 +0300

    Update build script

--- zImage.sh
+++ zImage.sh
@@ -3,6 +3,6 @@ cd ~/kernel-3.10
 mkdir out
 make O=out ARCH=arm64 CROSS_COMPILE=~/gcc/gcc-linaro-5.2-aarch64-linux-gnu/bin/aarch64-linux-gnu- P70_defconfig
 make -j2 O=out ARCH=arm64 CROSS_COMPILE=~/gcc/gcc-linaro-5.2-aarch64-linux-gnu/bin/aarch64-linux-gnu-
-cp ~/kernel-3.10/out/arch/arm64/boot/Image.gz-dtb ~/kernel-3.10/out/arch/arm64/boot/boot.img-kernel
-cp ~/kernel-3.10/out/arch/arm64/boot/boot.img-kernel ~/Kitchen/boot/boot.img-kernel
+cp ~/kernel-3.10/out/arch/arm64/boot/Image.gz-dtb ~/Kitchen/boot/boot.img-kernel
+cp ~/kernel-3.10/out/arch/arm64/boot/Image.gz-dtb ~/Kitchen/boot_vdt/boot.img-kernel
 cd
