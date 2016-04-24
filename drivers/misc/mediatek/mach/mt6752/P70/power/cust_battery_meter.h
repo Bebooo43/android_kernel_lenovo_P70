7cea9975fb1f429846607b21cd64a99820d8f8e7
vgdn1942 <vgdn1942@gmail.com>
2016-04-24 17:25:29 +0300

    Fix battery drop

--- drivers/misc/mediatek/mach/mt6752/P70/power/cust_battery_meter.h
+++ drivers/misc/mediatek/mach/mt6752/P70/power/cust_battery_meter.h
@@ -54,7 +54,7 @@
 
 /* battery meter parameter */
 #define CHANGE_TRACKING_POINT
-#define CUST_TRACKING_POINT  20
+#define CUST_TRACKING_POINT  1
 #define CUST_R_SENSE         68
 #define CUST_HW_CC 		    0
 #define AGING_TUNING_VALUE   103
