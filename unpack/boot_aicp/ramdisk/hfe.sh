db7195d9256584eb67bca8c0099e7c27e20907b7
vgdn1942 <vgdn1942@gmail.com>
2016-05-21 22:39:55 +0300

    Update hfe.sh

--- unpack/boot_aicp/ramdisk/hfe.sh
+++ unpack/boot_aicp/ramdisk/hfe.sh
@@ -3,14 +3,14 @@
 (while true; do
 
 if [ wake=`cat /sys/power/wait_for_fb_sleep` ]; then
-    HFE=`content query --uri content://settings/system --projection name:value --where "name='haptic_feedback_enabled'" | rev | cut -c1-1 | rev`
+    HFE=`settings get system haptic_feedback_enabled`
     if [ "$HFE" -ne "0" ]; then
-        content insert --uri content://settings/system --bind name:s:haptic_feedback_enabled --bind value:i:0
+        settings put system haptic_feedback_enabled 0
     fi
 fi
 if [ wake=`cat /sys/power/wait_for_fb_wake` ]; then 
     if [ "$HFE" -eq "1" ]; then
-        content insert --uri content://settings/system --bind name:s:haptic_feedback_enabled --bind value:i:1
+        settings put system haptic_feedback_enabled 1
     fi
 fi
 
